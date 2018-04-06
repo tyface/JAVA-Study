6f153eb955dc750b226ec0bdfcff3f309bf10706
Yu JiSang <zxuz34@gmail.com>
2018-04-06 18:37:12 +0900

    Add sort

---
+++ Day0405_Array/src/BubbleSort.java
@@ -0,0 +1,29 @@
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// 배열 오름차순 정렬 하기
+
+		int[] arr = { 3, 5, 4, 1, 2, 12, 4, 167,345,23416,166,34,43, 6, 47, 58, 33 }; // 정렬할 정수들은 배열에 저장
+
+		// 정렬을 시작하는 반복문_배열의 크기만큼 반복
+		for (int i = 1; i < arr.length; i++) {
+			// 상위 반복문에 인덱스에 해당하는 정수와 나머지 수들을 한번씩 비교하기위한 반복문
+			for (int j = 0; j < arr.length - i; j++) {
+				// 이번 반복회차의 인덱스에 저장되어있는 정수보다 작은수가 있다면 그 두정수의 값을 서로 바꾸는 소스
+				if (arr[j] > arr[j + 1]) {
+					int temp; // 정수의 자리를 바꿀때 사용할 임시변수
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+
+		// 출력
+		for (int e : arr) {
+			System.out.print(e + " ");
+		}
+	}
+
+}
