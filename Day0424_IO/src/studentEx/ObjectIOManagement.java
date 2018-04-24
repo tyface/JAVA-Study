f8fe2b3c171a18165c6e35c692ac76eafef4850f
Yu JiSang <zxuz34@gmail.com>
2018-04-24 16:52:47 +0900

    update studentEx

--- Day0424_IO/src/studentEx/ObjectIOManagement.java
+++ Day0424_IO/src/studentEx/ObjectIOManagement.java
@@ -2,6 +2,7 @@ package studentEx;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -9,34 +10,36 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-
 public class ObjectIOManagement {
 	private String filePath;
 	private ObjectInputStream in;
 	private ObjectOutputStream out;
+	private FileInputStream fis;
 
 	public ObjectIOManagement() {
 		this.filePath = "src/studentEx/StudentList.dat";
 		this.in = null;
 		this.out = null;
+		this.fis = null;
 	}
 
 	public MyList<Student> getList() {
 		MyList<Student> list = null;
 
 		try {
-
 			in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(filePath)));
-
 			list = (MyList<Student>) in.readObject();
 
 		} catch (FileNotFoundException e) {
 			System.out.println("파일이 없습니다.");
+		} catch (EOFException e) {
+			return new MyList<Student>();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} finally {
+
 			try {
 				if (in != null)
 					in.close();
