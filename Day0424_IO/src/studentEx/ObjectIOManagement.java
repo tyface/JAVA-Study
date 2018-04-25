dab718fac78518f12d6be9db384bc2645d876b34
Yu JiSang <zxuz34@gmail.com>
2018-04-25 14:16:16 +0900

    final student

--- Day0424_IO/src/studentEx/ObjectIOManagement.java
+++ Day0424_IO/src/studentEx/ObjectIOManagement.java
@@ -11,12 +11,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class ObjectIOManagement {
-	private String filePath; // 파일 경로
+	private static final String FILE_PATH = "src/studentEx/StudentList.dat"; // 파일 경로
 	private ObjectInputStream in;
 	private ObjectOutputStream out;
 
 	public ObjectIOManagement() {
-		this.filePath = "src/studentEx/StudentList.dat";
 		this.in = null;
 		this.out = null;
 	}
@@ -25,13 +24,14 @@ public class ObjectIOManagement {
 		MyList<Student> list = null;
 
 		try {
-			in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(filePath)));
+			in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(FILE_PATH)));
 			list = (MyList<Student>) in.readObject(); // 학생 리스트 객체를 불러와서 list변수에 삽입
 
 		} catch (FileNotFoundException e) {
 			System.out.println("파일이 없습니다.");
+			list = new MyList<Student>();
 		} catch (EOFException e) {
-			return new MyList<Student>();
+			list = new MyList<Student>();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -51,11 +51,10 @@ public class ObjectIOManagement {
 	public void pushList(MyList<Student> students) { // 학생 리스트 객체를 지정 파일에 삽입
 
 		try {
-			out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(filePath)));
+			out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(FILE_PATH)));
 
 			out.writeObject(students); // 학생 리스트 객체를 지정 파일에 삽입
 			out.flush(); // Output 초기화
-
 		} catch (FileNotFoundException e) {
 			System.out.println("파일이 없습니다.");
 		} catch (IOException e) {
